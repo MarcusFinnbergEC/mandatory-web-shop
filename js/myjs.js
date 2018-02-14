3c8739aacedfc896a8d233e9f5746ad9aabab9ed
Marcus Finnberg <marcus.finnberg@outlook.com>
2018-02-14 21:01:49 +0100

    Started with the array for my assorment and added images for the products

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+let productFabric = ["Bauer", "CCM", "Warrior"];
+let productName = ["Vapor 1X Lite Griptac", "Supreme 1S", "Re-Akt 200", "RibCor Trigger2 PMT Grip", "Super Tacks", "Fitlite 3DS", "Covert QRL SE Grip", "Alpha QX", "Covert PX+"];
+let productPic = [];
+let productPrice = [239, 169, 239, 229, 159, 178, 229, 155, 159];
+let productDesc = [];
+
+
